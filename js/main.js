88c5db4fd2030422325119e65d250db811f35c67
Flávio Félix <fau_felix30@hotmail.com>
2020-06-08 19:20:23 -0300

    Começando no javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Flávio Félix";
+var n1 = 29;
+var n2 = 5;
+var frase = "Japão melhor time do mundo"
+//alert(nome + " tem" + idade + "anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Alemanha"));
